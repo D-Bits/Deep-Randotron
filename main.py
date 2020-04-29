ea26fe7f8bab328f5f934be9b1642a0af87a1b1f
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-04-29 14:39:07 -0700

    Misc changes.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
         # Prompt the user to enter how many records worth of data they want to generate
         user_num = int(input("Please enter an integer (no commas) for how many records of test data you want to generate: "))
         gen_employees(user_num)
-        print(f'{user_num} record(s) successfully. Check the "data" directory for an "employees.csv" file ')
+        print(f'{user_num} record(s) successfully generated. Check the "data" directory for an "employees.csv" file ')
     except ValueError:
         print("Please enter an integer. Program terminated.")
 
