a4d378613b8483d7b70d4316d804ebfaa9f83a67
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-04-30 16:43:39 -0700

    Added mock data generation for servers.

--- generators.py
+++ generators.py
@@ -7,26 +7,28 @@ from csv import DictWriter
 options = {
     0: "Return to Main Menu.",
     1: "Generate Mock Data for Employees.",
+    2: "Generate Mock Data for Servers."
 }
 
 
 # Generate an arbitrary number of fake employees, and write them to a CSV
-def gen_employees_csv(emp_num):
+def gen_employees_csv(records_num):
 
     emp = open('data/employees.csv', 'w', newline='')
-    fields = ['last_name','first_name','email','password_hash','phone','home_address','city','state','zip']
+    fields = ['last_name','first_name','job','email','password_hash','phone','home_address','city','state','zip']
     writer = DictWriter(emp, fieldnames=fields)
     # Write the columns to file
     writer.writeheader()
     
     fake = Faker()
 
-    for i in range(emp_num):
+    for i in range(records_num):
 
         writer.writerow(
             {
                 'last_name': fake.last_name(),
                 'first_name': fake.first_name(),
+                'job': fake.job(),
                 'email': fake.email(),
                 'password_hash': fake.sha256().upper(),
                 'phone': fake.phone_number(),
@@ -34,6 +36,30 @@ def gen_employees_csv(emp_num):
                 'city': fake.city(),
                 'state': fake.state(),
                 'zip': fake.random_int(11111, 99999),
+            }
+        )
+
+
+# Generate fake information about servers
+def gen_servers(records_num):
+
+    servers = open('data/servers.csv', 'w', newline='')
+    fields = ['hostname', 'ip_public', 'ip_private', 'mac_address', 'port']
+    writer = DictWriter(servers, fieldnames=fields)
+    # Write the columns to file
+    writer.writeheader()
+
+    fake = Faker()
+
+    for i in range(records_num):
+
+        writer.writerow(
+            {
+                'hostname': fake.hostname(),
+                'ip_public': fake.ipv4_public(),
+                'ip_private': fake.ipv4_private(),
+                'mac_address': fake.mac_address(),
+                'port': fake.port_number(),
             }
         )
 
@@ -48,11 +74,14 @@ def generators_main():
     print()
 
     u_choice = int(input("Enter a choice, based on the above options: "))
+    user_num = int(input("Please enter the number of records you want to generate: "))
 
-    if u_choice == 1:
-        user_num = int(input("Please enter the number of records you want to generate: "))
+    if u_choice == 1:   
         gen_employees_csv(user_num)
         print(f'{user_num} record(s) successfully generated. Check the "data" directory for an "employees.csv" file ')
+    elif u_choice == 2:
+        gen_servers(user_num)
+        print(f'{user_num} record(s) successfully generated. Check the "data" directory for an "servers.csv" file ')
     else:
         print("***Please enter a valid int, from the below options.***")
         generators_main()
