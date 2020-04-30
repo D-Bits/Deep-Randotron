96126f663f0409c049c8f890d62ad184e91cfad2
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-04-29 18:05:22 -0700

    Renamed "employees" to "generators".

--- generators.py
+++ generators.py
@@ -7,7 +7,7 @@ from csv import DictWriter
 def gen_employees_csv(emp_num):
 
     emp = open('data/employees.csv', 'w', newline='')
-    fields = ['last_name','first_name','email','emp_password', 'phone', 'home_address','city','state', 'zip']
+    fields = ['last_name','first_name','email','password_hash','phone','home_address','city','state','zip']
     writer = DictWriter(emp, fieldnames=fields)
     # Write the columns to file
     writer.writeheader()
@@ -21,12 +21,12 @@ def gen_employees_csv(emp_num):
                 'last_name': fake.last_name(),
                 'first_name': fake.first_name(),
                 'email': fake.email(),
-                'emp_password': fake.sha256().upper(),
+                'password_hash': fake.sha256().upper(),
                 'phone': fake.phone_number(),
                 'home_address': fake.street_address(),
                 'city': fake.city(),
                 'state': fake.state(),
-                'zip': fake.random_int(00000, 99999),
+                'zip': fake.random_int(11111, 99999),
             }
         )
 
